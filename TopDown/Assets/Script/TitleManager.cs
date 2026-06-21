209bed17931724a35c60563484c87ac9e673bdbf
hyemin7486 <kasanom00@naver.com>
2026-06-21 15:11:07 +0900

    타이틀 신 이미지 교체, 게임 종료 버튼 생성

--- TopDown/Assets/Script/TitleManager.cs
+++ TopDown/Assets/Script/TitleManager.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class TitleManager : MonoBehaviour
 {
-   public void GameStartButton()
+    public void StartGame()
     {
         GameManager.Instance.StartGame();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
